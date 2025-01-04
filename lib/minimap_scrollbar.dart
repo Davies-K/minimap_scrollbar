4b54c7bf424a6960d7caff751bcd968c0bd15ec8
Davies Kwarteng <d.opoku-kwarteng@q-cells.com>
2025-01-04 04:49:57 +0100

    address comments

--- lib/minimap_scrollbar.dart
+++ lib/minimap_scrollbar.dart
@@ -1,3 +1,5 @@
-library minimap_scrollbar;
+/// Flutter package for a minimap scrollbar.
+///  - A minimap scrollbar is a scrollbar that shows a preview of the entire scrollable area.
+library;
 
 export 'src/widgets/minimap_scrollbar_widget.dart';
